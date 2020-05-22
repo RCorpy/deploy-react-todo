020586b9574345deca8911382adf5f24f27d4690
RamonNav <belamst@hotmail.com>
2020-05-22 03:04:33 +0200

    Updates

---
+++ precache-manifest.3960e3142facf27af57faae7e5ee82e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3aa58e5c5515b9eb5bd6baff7e50e89",
+    "url": "/deploy-react-todo/index.html"
+  },
+  {
+    "revision": "6cd8ba5d97e4ebccda68",
+    "url": "/deploy-react-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9c391f33f344c46d55b3",
+    "url": "/deploy-react-todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6cd8ba5d97e4ebccda68",
+    "url": "/deploy-react-todo/static/js/2.2177db93.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/deploy-react-todo/static/js/2.2177db93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c391f33f344c46d55b3",
+    "url": "/deploy-react-todo/static/js/main.db04c642.chunk.js"
+  },
+  {
+    "revision": "4d8caa193333be156a95",
+    "url": "/deploy-react-todo/static/js/runtime-main.ddefcc1d.js"
+  },
+  {
+    "revision": "8db7e601ce13f22cb9b8cc33c114b2da",
+    "url": "/deploy-react-todo/static/media/reymon.8db7e601.png"
+  }
+]);
